5e09adc6de204c7861f68fde0a67b3c452682a85
sumaihfahad <sosophysics@gmail.com>
2019-08-26 01:38:06 +0300

    first

---
+++ first week.py
@@ -0,0 +1,109 @@
+print('hello world')
+if 5 > 3 :
+	print('five is qreater than two')
+
+#this is a comment
+
+print('Sumaih') #this is my name
+
+#print('Hi')
+'''
+hello evry one .
+my name is Sumaih 
+I'm happy
+'''
+
+x = 5
+y = 'Sumaih'
+print(x)
+print(y)
+print(x,y)
+
+x = 'Amal'
+print(x)
+
+x , y , z ='Orange' , 'Banana','Cherry'
+print(x,y,z)
+
+x=y=z='Orange'
+print(x,y,z)
+
+x = 'awesome'
+print("python is" + x)
+print("python is" , x)
+
+x = 'puthon is'
+y = 'awesome'
+z = x + y
+print(z)
+
+
+x = 5
+y = 10
+print(x+y)
+
+x = 5
+y = 'John'
+#print(x+y)
+print(x,y)
+
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x=1
+y=3456789876765
+z=-326788
+print(type(x))
+print(type(y))
+print(type(z))
+
+x= 1.0
+y=1.1
+z=-35.65
+print(type(x))
+print(type(y))
+print(type(z))
+
+x=32e4
+y=12E4
+z=-34.4e100
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+x=3+5j
+y=5j
+z=-5j
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+x=1 # int
+y=2.8 #float
+z=1j # complex
+
+a = float(x)  # convert from int (x=1) to float
+b = int(y) # convert from float (y=2.8) to int
+c = complex(x) # convert from int (x=1) to complex
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+
+
+import random
+print(random.randrange(1,10))
+
+print(random.randrange(1,10))
+
+print(random.randrange(1,10))
+
+
+
+
+
+
