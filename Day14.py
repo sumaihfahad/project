c843ba5a1d3d83462ee7c4c3c312702aec13b5f6
sumaihfahad <54503351+sumaihfahad@users.noreply.github.com>
2019-09-02 19:38:10 +0300

    D14W2

---
+++ Day14.py
@@ -0,0 +1,26 @@
+# Day 14..
+
+fruits = ['apple','orang','banana']
+numbers = [1,2,3,4,5,6,7,8]
+floatNumbers = [1.2,3.4,1.0,5.5,0.0]
+
+# print rang of items:
+print(numbers[0:3])
+
+# check if item esists :
+if 'apple' in fruits :
+	print('yes, apple in list')
+
+if 'apple' and 'cherry' in fruits :
+	print('yes, apple and orang in list')
+else :
+	print('):')
+
+# repeat item:
+list4 = ['hi']*4
+print(list4) 
+
+# operator (+)
+list5 = floatNumbers + numbers
+print(list5)
+
